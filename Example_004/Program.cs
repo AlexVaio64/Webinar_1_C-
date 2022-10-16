d2a274ff557b6fe26b3868d24cbbc1f049f133c6
Michel Nekrasov <moscou@inbox.ru>
2022-10-17 00:36:18 +0300

    add Example 004

---
+++ Example_004/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа принимает на вход одно число (N), а на выходе показывает все
+// целые числа в промежутке от -N до N.
+
+Console.WriteLine("Введите любое целое число: ");
+int N = int.Parse(Console.ReadLine()!);
+int M = N * (-1);
+
+if (N < 0)
+{
+    int temp = N;
+    N = M;
+    M = temp;
+}
+    while (M <= N)
+{
+    Console.Write(M + " ");
+    M = M + 1;
+}
