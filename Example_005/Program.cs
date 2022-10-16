80806fcecdd13c984a4592a950cd6c8d42937810
Michel Nekrasov <moscou@inbox.ru>
2022-10-17 00:49:01 +0300

    add Example 005

---
+++ Example_005/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа принимает на вход трёхзначное число и на выходе показывает последнюю
+// цифру этого числа.
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+// int n = number%10
+Console.WriteLine("Остаток " + number%10);
