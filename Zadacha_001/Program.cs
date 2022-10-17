51c12ffc36b784a9a8eee9d69cd7182ce9b82a66
Michel Nekrasov <moscou@inbox.ru>
2022-10-17 18:16:24 +0300

    add Zadacha 001

---
+++ Zadacha_001/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, которая на вход принимает два целых числа и выдаёт,
+// какое число большее, а какое меньшее.
+Console.WriteLine("Введите число 1: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число 2: ");
+int b = int.Parse(Console.ReadLine()!);
+if(a > b) 
+{
+        Console.WriteLine("max = " + a + " min = " + b);
+}
+else
+{    
+    Console.WriteLine("max = " + b + " min = " + a);
+}
